50514d24befa56b14f1971a63e1d1d8ac9b2fe3d
fong1980 <fong1980@gmail.com>
2018-06-02 20:23:02 +0200

    checkbox pizzabase

--- src/component/pizzaBase.js
+++ src/component/pizzaBase.js
@@ -1,10 +1,68 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, Component } from "react";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
+//import { pickBase } from "../actions/counter";
+
+// this.props.increment();
+// console.log(this.counter, "test");
 
 class PizzaBase extends PureComponent {
+  constructor(props) {
+    super(props);
+    //this.state = {};
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    //console.log(this.state.isGoing);
+    //console.log(this.props.pickBase(event.target.value));
+    //this.props.pickBase(event.target.value);
+    console.log(event.target.value);
+
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+  }
+
   render() {
-    return <div className="Square">chose toppings</div>;
+    return (
+      <form>
+        <label>
+          <input
+            name="choseBase"
+            type="radio"
+            value="8,99"
+            onChange={this.handleInputChange}
+          />25cm NY Style € 8,99
+          <input
+            name="choseBase"
+            type="radio"
+            value="11,49"
+            onChange={this.handleInputChange}
+          />30cm NY Style € 11,49
+          <input
+            name="choseBase"
+            type="radio"
+            value="13,49"
+            onChange={this.handleInputChange}
+          />35cm NY Style € 13,49
+          <input
+            name="choseBase"
+            type="radio"
+            value="6,45"
+            onChange={this.handleInputChange}
+          />20cm NY Style € 6,45
+        </label>
+        <br />
+      </form>
+    );
   }
 }
+const mapStateToProps = state => {
+  return {
+    counter: state.counter
+  };
+};
 
-export default PizzaBase;
+export default connect(mapStateToProps, {})(PizzaBase);
