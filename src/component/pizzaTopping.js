5b0ce20372f04bc5e144ac48534e45038cd55120
fong1980 <fong1980@gmail.com>
2018-06-03 18:16:34 +0200

    try again

--- src/component/pizzaTopping.js
+++ src/component/pizzaTopping.js
@@ -7,6 +7,7 @@ class PizzaTopping extends PureComponent {
     super(props);
     this.state = {
       info: [],
+      disable: [0, 0, 0, 0, 0, 0],
       checked: "",
 
       isGoing: true,
@@ -18,10 +19,24 @@ class PizzaTopping extends PureComponent {
   }
 
   handleInputChange(event) {
-    this.state.info.push(event.target.name);
+    if (!this.state.info.includes(event.target.name)) {
+      //nog niet in de array? dan toevoegen
+      console.log("bestaat nog niet");
+      this.state.info.push(event.target.name);
+    } else {
+      // this.state.info
+      // var index=
 
-    console.log(this.state.info.length);
-    console.log(this.state.info);
+      var array = [2, 5, 9];
+      var index = array.indexOf(5);
+      if (index > -1) {
+        array.splice(index, 1);
+      }
+    }
+
+    // console.log(this.state.info.includes("2"), "  bestaat  ");
+    // console.log(this.state.info.length);
+    // console.log(this.state.info);
     this.props.addTopping(Number(event.target.value));
   }
 
@@ -33,36 +48,42 @@ class PizzaTopping extends PureComponent {
           type="checkbox"
           name="1"
           value="0.5"
+          disabled={this.state.disable[0]}
           onChange={this.handleInputChange}
         />Pineapple<br />
         <input
           type="checkbox"
           name="2"
           value="0.5"
+          disabled={this.state.disable[1]}
           onChange={this.handleInputChange}
         />Corn<br />
         <input
           type="checkbox"
           name="3"
           value="0.5"
+          disabled={this.state.disable[2]}
           onChange={this.handleInputChange}
         />Olives (green)<br />
         <input
           type="checkbox"
           name="4"
           value="0.5"
+          disabled={this.state.disable[3]}
           onChange={this.handleInputChange}
         />Spinach<br />
         <input
           type="checkbox"
           name="5"
           value="0.5"
+          disabled={this.state.disable[4]}
           onChange={this.handleInputChange}
         />Cherry tomatoes<br />
         <input
           type="checkbox"
           name="6"
           value="0.5"
+          disabled={this.state.disable[5]}
           onChange={this.handleInputChange}
         />Chicken<br />
       </div>
