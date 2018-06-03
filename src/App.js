d3c437f51ae6114c4193b412e1bb6dec64f69bf6
fong1980 <fong1980@gmail.com>
2018-06-03 17:22:37 +0200

    got a array to the state, let try something new

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ class App extends Component {
         <PizzaTopping />
         <PizzaDrone />
         <PizzaTotalPrice />
-        <Test />
       </div>
     );
   }
--- src/component/pizzaTopping.js
+++ src/component/pizzaTopping.js
@@ -5,11 +5,23 @@ import { addTopping } from "../actions/pickAction";
 class PizzaTopping extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      info: [],
+      checked: "",
+
+      isGoing: true,
+      numberOfGuests: 2,
+      count: 0
+    };
+
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleInputChange(event) {
+    this.state.info.push(event.target.name);
+
+    console.log(this.state.info.length);
+    console.log(this.state.info);
     this.props.addTopping(Number(event.target.value));
   }
 
@@ -19,37 +31,37 @@ class PizzaTopping extends PureComponent {
         <p> Chose max 3 topping </p>
         <input
           type="checkbox"
-          name="vehicle"
+          name="1"
           value="0.5"
           onChange={this.handleInputChange}
         />Pineapple<br />
         <input
           type="checkbox"
-          name="vehicle"
+          name="2"
           value="0.5"
           onChange={this.handleInputChange}
         />Corn<br />
         <input
           type="checkbox"
-          name="vehicle"
+          name="3"
           value="0.5"
           onChange={this.handleInputChange}
         />Olives (green)<br />
         <input
           type="checkbox"
-          name="vehicle"
+          name="4"
           value="0.5"
           onChange={this.handleInputChange}
         />Spinach<br />
         <input
           type="checkbox"
-          name="vehicle"
+          name="5"
           value="0.5"
           onChange={this.handleInputChange}
         />Cherry tomatoes<br />
         <input
           type="checkbox"
-          name="vehicle"
+          name="6"
           value="0.5"
           onChange={this.handleInputChange}
         />Chicken<br />
@@ -63,20 +75,5 @@ const mapStateToProps = state => {
 };
 export default connect(mapStateToProps, { addTopping })(PizzaTopping);
 
-// var limit = 3;
-// $('input.single-checkbox').on('change', function(evt) {
-//    if($(this).siblings(':checked').length >= limit) {
-//        this.checked = false;
-//    }
-// });
-
-// <div class="pricing-levels-3">
-// <p><strong>Which level would you like? (Select 3 Levels)</strong></p>
-// <input class="single-checkbox"type="checkbox" name="vehicle" value="Bike">Level 1<br>
-// <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 2<br>
-// <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 3<br>
-// <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 4<br>
-// <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 5<br>
-// <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 6<br>
-// <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 7<br>
-// </div>
+// checked="true"
+// disabled="true"
